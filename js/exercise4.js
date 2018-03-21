16832d79c6b6c047ad9454834581eb18b08abb42
smithalex24 <smith25243@gmail.com>
2018-03-21 16:16:40 -0700

    primitives

---
+++ js/exercise4.js
@@ -0,0 +1,8 @@
+
+
+
+var friends = 'Moe,Larry,Curly,Jane,Emma,Elizabeth,Elinor,Mary,Darcy,Grey,Lydia,Harriet';
+
+let friendsArray = friends.split(',');
+
+console.log(friendsArray.unsort());
---
+++ js/exercise9.js
@@ -0,0 +1,7 @@
+//9.) Change the word "Space" to the word "Spice".
+
+var word = "Space";
+
+word.replace("a", "i");
+
+word;
